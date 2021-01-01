09d6108817210e8326e1572cf14115f3288e5519
akapikumin <akapikumin100piki@gmail.com>
2021-01-01 10:24:01 +0900

    timedisplayに5000兆円フォントを反映した

--- js_folder/index.js
+++ js_folder/index.js
@@ -3,9 +3,10 @@
 function nowTimeDisplay(){
     let nowTime = new Date();
     document.getElementById('now').textContent = nowTime;
-    document.getElementById('hours').textContent = nowTime.getHours();
-    document.getElementById('minutes').textContent = nowTime.getMinutes();
-    document.getElementById('seconds').textContent = nowTime.getSeconds();
+    document.getElementById('hours').innerHTML = ('<span class="number-5000-i" title="' + ('00' + nowTime.getHours()).slice(-2) + '">' + ('00' + nowTime.getHours()).slice(-2) + '</span>');
+    document.getElementById('minutes').innerHTML = ('<span class="number-5000-i" title="' + ('00' + nowTime.getMinutes()).slice(-2) + '">' + ('00' + nowTime.getMinutes()).slice(-2) + '</span>');
+    document.getElementById('seconds').innerHTML = ('<span class="number-5000-i" title="' + ('00' + nowTime.getSeconds()).slice(-2) + '">' + ('00' + nowTime.getSeconds()).slice(-2) + '</span>');
+
     setTimeout(nowTimeDisplay, 100);
 }
 
